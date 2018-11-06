6a2989429c3abcbaef83192c3b99a36ca4bdc7e7
thiagomedinacc <thiago.medina@acad.pucrs.br>
2018-11-06 21:18:23 -0200

    melhora nomes das funções

--- T2Func/ExemplosComandos.hs
+++ T2Func/ExemplosComandos.hs
@@ -13,14 +13,14 @@ while(nro > 1) {
 }
 -}
 
-memoriaComValores =   update (update initial "res" 1) "nro" 5
+attMemoria =   update (update initial "res" 1) "nro" 5
 
-verificaLoop = MaiorQue (V "nro") (L 1)
+veriCondicao = MaiorQue (V "nro") (L 1)
 atribMult = Atrib "res" (Mult (V "res") (V "nro"))
 decrementaNro = Atrib "nro" (Sub (V "nro") (L 1)) 
 
-fatorial = While verificaLoop (Sequencia atribMult decrementaNro) --(evalComando atribResENro initial )
-resFinal = value (evalComando fatorial memoriaComValores) "res" 
+fatorial = While veriCondicao (Sequencia atribMult decrementaNro) 
+resFinal = value (evalComando fatorial attMemoria) "res" 
 
 
 -- Segundo comando: Multiplicaçao com auxilio de somas
@@ -34,14 +34,14 @@ while (nro2 > 0){
 }
 -}
 
-memoriaComValores2 = update (update (update initial "nro1" 4) "nro2" 6) "res" 0
+attMemoria2 = update (update (update initial "nro1" 4) "nro2" 6) "res" 0
 
-verificaLoop2 = MaiorQue (V "nro2") (L 0)
+veriCondicao2 = MaiorQue (V "nro2") (L 0)
 atribSoma = Atrib "res" (Add (V "res") (V "nro1"))
 decrementaNro2 = Atrib "nro2" (Sub (V "nro2") (L 1))
 
-multComSomas = While verificaLoop2 (Sequencia atribSoma decrementaNro2 )
-resFinal2 = value (evalComando multComSomas memoriaComValores2) "res"
+multComSomas = While veriCondicao2 (Sequencia atribSoma decrementaNro2 )
+resFinal2 = value (evalComando multComSomas attMemoria2) "res"
 
 
 -- Terceiro comando: Potencia com expoentes naturais:
@@ -56,14 +56,14 @@ while (exp > 0){
   
 -}
 
-memoriaComValores3 = update (update (update initial "base" 3) "exp" 4) "res" 1
+attMemoria3 = update (update (update initial "base" 3) "exp" 4) "res" 1
 
-verificaLoop3 = MaiorQue (V "exp") (L 0)
+veriCondicao3 = MaiorQue (V "exp") (L 0)
 atribMult2 = Atrib "res" (Mult (V "res") (V "base"))
 decrementaExp = Atrib "exp" (Sub (V "exp") (L 1))
 
-potComExpNat = While verificaLoop3 (Sequencia atribMult2 decrementaExp)
-resFinal3 = value (evalComando potComExpNat memoriaComValores3) "res"
+potComExpNat = While veriCondicao3 (Sequencia atribMult2 decrementaExp)
+resFinal3 = value (evalComando potComExpNat attMemoria3) "res"
 
 
 --Quarto comando: Divisão inteira:
@@ -72,10 +72,10 @@ dividendo = 15;
 divisor = 4;
 res = dividendo / divisor; (/ é o `div` do haskell)
 -}
-memoriaComValores4 = update (update initial "dividendo" 15) "divisor" 4 
+attMemoria4 = update (update initial "dividendo" 15) "divisor" 4 
 
 atribDivisao = Atrib "res" (Div (V "dividendo") (V "divisor"))
-resFinal4 = value (evalComando atribDivisao memoriaComValores4) "res"
+resFinal4 = value (evalComando atribDivisao attMemoria4) "res"
 
 
 -- Quinto comando: Máximo divisor comum (MDC)
@@ -93,17 +93,17 @@ while (n1 != n2){
 res = n1;
 -}
 
-memoriaComValores5 = update (update initial "n1" 360) "n2" 80
+attMemoria5 = update (update initial "n1" 360) "n2" 80
 
-verificaLoop5 = Diferente (V "n1") (V "n2")
+veriCondicao5 = Diferente (V "n1") (V "n2")
 verificaQualOMaior = MaiorQue (V "n1") (V "n2") -- Retorna True se "n1" maior que "n2", ou False, caso contrário.
 decrementaN1 = Atrib "n1" (Sub (V "n1") (V "n2"))
 decrementaN2 = Atrib "n2" (Sub (V "n2") (V "n1"))
 comandoDeDecisao = Escolha verificaQualOMaior  decrementaN1 decrementaN2
 
-mdc = While verificaLoop5 comandoDeDecisao
+mdc = While veriCondicao5 comandoDeDecisao
 
-resFinal5 = value (evalComando mdc memoriaComValores5) "n1"
+resFinal5 = value (evalComando mdc attMemoria5) "n1"
 
 
 
